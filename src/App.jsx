50113686d36e565ccdd5bb6f24034081e74ddb9e
Asanobm <kim_bumin@fourglobe.co.jp>
2020-10-11 21:23:45 +0900

    update: packages

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Layout } from 'antd'
+import React from 'react'
+
+function App() {
+  return (
+    <Layout>
+      <div>
+        <span>hello</span>
+      </div>
+    </Layout>
+  )
+}
+
+export default App
